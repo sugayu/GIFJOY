5c4e694f650d33de6fe25798426b7f69c46137d4
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-03-22 11:59:39 +0900

    Finish login singnup view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'posts#index'
+  devise_for :users
+  resources :users, only: [:show]
   resources :posts
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
